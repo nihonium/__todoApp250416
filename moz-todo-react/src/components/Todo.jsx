3815877c87e0af0142dce752624dcef866940413
Hironori Niho <hironori103@gmail.com>
2025-04-18 16:39:40 +0900

    webフォント追加

--- moz-todo-react/src/components/Todo.jsx
+++ moz-todo-react/src/components/Todo.jsx
@@ -60,14 +60,14 @@ function Todo(props) {
       </div>
       <div className="btn-group">
         <button type="button" className="btn" onClick={() => setEditing(true)}>
-          Edit <span className="visually-hidden">{props.name}</span>
+          編集する <span className="visually-hidden">{props.name}</span>
         </button>
         <button
           type="button"
           className="btn btn__danger"
           onClick={() => props.deleteTask(props.id)}
         >
-          Delete <span className="visually-hidden">{props.name}</span>
+          削除 <span className="visually-hidden">{props.name}</span>
         </button>
       </div>
     </div>
