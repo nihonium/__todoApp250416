0836b9cfb03156222e9760921396ca987bfb31f5
Hironori Niho <hironori103@gmail.com>
2025-04-18 20:25:54 +0900

    バリデーション追加

--- moz-todo-react/src/components/Form.jsx
+++ moz-todo-react/src/components/Form.jsx
@@ -1,16 +1,38 @@
 import { useState } from "react";
+// import { useForm } from 'react-hook-form';
 
 function Form(props) {
     const [name, setName] = useState("");
+    const [nameError, setNameError] = useState('')
+
+    const validateName = (value) => {
+        if (!value) return '値を入力してください。';
+        if (value.length > 15) return '15文字以内で入力してください';
+        return '';
+    }
 
     function handleChange(event) {
-        setName(event.target.value);
+        const value = event.target.value;
+        setName(value);
+        setNameError(validateName(value));
     }
+
+    const handleBlur = (e) => {
+        const error = validateName(e.target.value);
+        setNameError(error);
+        if (error) return;
+    };
 
     function handleSubmit(event) {
         event.preventDefault();
+        const error = validateName(name);
+        if (error) {
+            setNameError(error);
+            return;
+        }
         props.addTask(name);
         setName("");
+        setNameError('');
     }
 
     return (
@@ -27,9 +49,12 @@ function Form(props) {
                 name="text"
                 autoComplete="off"
                 value={name}
+                onBlur={handleBlur}
                 onChange={handleChange}
+                placeholder="タスクを入力してください"
             />
-            <button type="submit" className="btn btn__primary btn__lg">
+            {nameError && <p>{nameError}</p>}
+            <button type="submit" className="btn btn__primary btn__lg" disabled={nameError}>
                 追加
             </button>
       </form>
