9813daed90d6ad8589a743846a0b1f0f38654364
Hironori Niho <hironori103@gmail.com>
2025-05-09 16:59:22 +0900

    localStorage追加

--- moz-todo-react/src/main.jsx
+++ moz-todo-react/src/main.jsx
@@ -3,11 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-const DATA = [
-  { id: "todo-0", name: "食べる", completed: true },
-  { id: "todo-1", name: "寝る", completed: false },
-  { id: "todo-2", name: "繰り返す", completed: false },
-];
+const DATA = [];
+
+if (localStorage.getItem('tasks') === null) {
+  localStorage.setItem('tasks', JSON.stringify(DATA));
+} else {
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
+  for (let i = 0; i < tasks.length; i++) {
+    DATA.push(tasks[i]);
+  }
+  localStorage.setItem('tasks', JSON.stringify(DATA));
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
